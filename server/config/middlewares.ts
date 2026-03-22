66fa50c43dc885eda5a9c15a6bf9784cc4e31940
thanu26-07 <thanujabhupalam@gmail.com>
2026-03-22 16:16:49 +0530

    middleware updated

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,10 +1,36 @@
+// import type { Core } from '@strapi/strapi';
+
+// const config: Core.Config.Middlewares = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+// export default config;
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://fitness-tracker-48kv.vercel.app',
+        'http://localhost:5173'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
